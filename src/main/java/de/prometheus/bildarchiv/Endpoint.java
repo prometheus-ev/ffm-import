0d47fde19e0eb13e48ada664cd6bfbcf08437876
Mihail Atanassov <atanassov.mihail@gmail.com>
2017-04-10 16:03:03 +0200

    Add Endpoint enum

---
+++ src/main/java/de/prometheus/bildarchiv/Endpoint.java
@@ -0,0 +1,74 @@
+package de.prometheus.bildarchiv;
+
+import java.util.Properties;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import de.prometheus.bildarchiv.exception.ResumptionTokenNullException;
+
+public enum Endpoint {
+
+	ENTITIES("entities?"), KINDS("kinds?"), RELATIONS("relations?"), RELATIONSHIPS("relationships?");
+
+	private final String name;
+	private final String prefix = "&metadataPrefix=kor";
+	private final String listRecords = "verb=ListRecords";
+	private final String listIdentifiers = "verb=ListIdentifiers";
+	private final String getRecord = "verb=GetRecord&identifier=";
+	private final String resumptionToken = "&resumptionToken=";
+
+	private String baseUrl = "";
+	private String apiKey = "&api_key=";
+
+	private static final Logger logger = LogManager.getLogger(Endpoint.class);
+	private static Properties properties;
+
+	Endpoint(String name) {
+		this.name = name;
+		// init();
+	}
+
+	public String getRecord(final String identifier) {
+		if (identifier == null)
+			return null;
+		return baseUrl + name + getRecord + identifier + prefix + apiKey;
+	}
+
+	public String listRecords() {
+		return baseUrl + name + listRecords + prefix + apiKey;
+	}
+
+	public String listRecords(final String token) throws ResumptionTokenNullException {
+		if (token == null)
+			throw new ResumptionTokenNullException();
+		return baseUrl + name + listRecords + prefix + apiKey + resumptionToken + token;
+	}
+
+	public String listIdentifiers() {
+		return baseUrl + name + listIdentifiers + prefix + apiKey;
+	}
+
+	public String listIdentifiers(final String token) {
+		if (token == null)
+			return null;
+		return baseUrl + name + listIdentifiers + prefix + apiKey + resumptionToken + token;
+	}
+
+	private void init() {
+		final String propertiesFile = "src/main/resources/properties/endpoint.properties";
+		if (properties == null) {
+			properties = new Properties();
+			try {
+				properties.load(Endpoint.class.getResourceAsStream(propertiesFile));
+			} catch (Exception e) {
+				e.printStackTrace();
+				logger.error("Unable to load api key from " + propertiesFile, e);
+				System.exit(1);
+			}
+		}
+		this.apiKey += (String) properties.get("apiKey");
+		this.baseUrl = (String) properties.get("baseUrl");
+	}
+
+}
