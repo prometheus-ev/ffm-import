c3d79cd3ba2ee2c3a3a5489ebc615ff38774558c
Mihail Atanassov <atanassov.mihail@gmail.com>
2017-06-16 11:49:01 +0200

    Add comments

--- src/main/java/de/prometheus/bildarchiv/exception/ResumptionTokenNullException.java
+++ src/main/java/de/prometheus/bildarchiv/exception/ResumptionTokenNullException.java
@@ -1,14 +1,26 @@
 package de.prometheus.bildarchiv.exception;
 
+/**
+ * 
+ * @author matana
+ *
+ */
 public class ResumptionTokenNullException extends Exception {
 
 	private static final long serialVersionUID = 6928774210377441154L;
 	
+	/**
+	 * 
+	 */
 	public ResumptionTokenNullException() {
 		super();
 	}
 	
-	public ResumptionTokenNullException(String message) {
+	/**
+	 * 
+	 * @param message
+	 */
+	public ResumptionTokenNullException(final String message) {
 		super(message);
 	}
 
